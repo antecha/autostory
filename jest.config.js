d05023d73a26fe43a5b68004cd1d869724b99017
AndreasT <andreas.teneketzoglou@medwing.com>
2020-02-28 14:27:56 +0100

    chore: ignore stories for jest coverage

--- jest.config.js
+++ jest.config.js
@@ -2,22 +2,22 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testEnvironment: "jest-environment-jsdom",
-  testMatch: ["<rootDir>/src/**/?(*.)+(test).js?(x)"],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['<rootDir>/src/**/?(*.)+(test).js?(x)'],
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.js?(x)"],
-  coverageDirectory: "coverage",
-  coverageReporters: ["text-summary", "text", "json-summary", "lcov"],
+  collectCoverageFrom: ['<rootDir>/src/**/*.js?(x)', '!<rootDir>/**/*.stories.js'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text-summary', 'text', 'json-summary', 'lcov'],
   coverageThreshold: {
     global: {
-      lines: 90
-    }
+      lines: 90,
+    },
   },
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|sass|less)$": "<rootDir>/__mocks__/styleMock.js"
-  }
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|sass|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
 };
